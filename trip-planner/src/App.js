d766d409a403597c89f93d0bb33bea42b26f88a1
harshu.varanasi9 <harshu.varanasi9@gmail.com>
2025-01-25 23:30:44 -0500

    merging changes for now

--- trip-planner/src/App.js
+++ trip-planner/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import UserInput from './pages/user_input.jsx';
 import HomePage from './pages/home_page.jsx';
+import TripDetails from './pages/trip_details.jsx';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/user_input" element={<UserInput />} />
+      <Route path="/trip_details" element={<TripDetails></TripDetails>}></Route>
     </Routes>
   </BrowserRouter>
     
--- trip-planner/src/pages/trip_details.jsx
+++ trip-planner/src/pages/trip_details.jsx
@@ -2,12 +2,11 @@
 import React from 'react';
 //import './HomePage.css';
 
-import './home_page.css';
-import './user_input.jsx';
+import './trip_details.css';
 //import { useNavigate } from "react-router-dom";
 import { BrowserRouter as Router } from 'react-router-dom';
 
-const HomePage = () => {
+const TripDetails = () => {
 
     function handleNavigation() {
         window.location.href = '/user_input';
@@ -17,16 +16,12 @@ const HomePage = () => {
     <div className="home-page">
       <main className="main-content">
         <section className="hero">
-          <h1>Welcome to the Trip Planner</h1>
-          <p>Discover amazing places, curated just for you.</p>
+          <p>the output page</p>
         </section>
 
-        <section>
-            <button className="cta-button" onClick={handleNavigation}> Get More Specific Suggestions</button>
-        </section>
       </main>
     </div>
   );
 };
 
-export default HomePage;
+export default TripDetails;
